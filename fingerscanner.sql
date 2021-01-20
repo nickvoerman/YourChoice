f37ae57d6b3ca7b3c34b8d3f8f7c435f629ec43a
NICK VOERMAN <nick21092001@gmail.com>
2021-01-20 15:32:25 +0100

    Refactored the database

--- fingerscanner.sql
+++ fingerscanner.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 20 jan 2021 om 15:06
--- Serverversie: 10.4.11-MariaDB
--- PHP-versie: 7.4.5
+-- Gegenereerd op: 20 jan 2021 om 15:32
+-- Serverversie: 10.4.17-MariaDB
+-- PHP-versie: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -63,8 +63,26 @@ CREATE TABLE `klant` (
 --
 
 INSERT INTO `klant` (`id`, `naam`, `box`, `gezin`, `allergie`, `voorkeuren`, `vingerpint`, `voedselbank`) VALUES
-(0, 'Younes', '2', 3, 'pinda', 'chocola', '123', '2'),
+(0, 'Younes', '2', 3, 'pinda', 'chocola', '224234', '2'),
 (1, 'Younes', '2', 3, 'pinda', 'chocola', '123', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `vingerid`
+--
+
+CREATE TABLE `vingerid` (
+  `id` int(11) NOT NULL,
+  `vinger_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `vingerid`
+--
+
+INSERT INTO `vingerid` (`id`, `vinger_id`) VALUES
+(1, 224234);
 
 -- --------------------------------------------------------
 
@@ -96,6 +114,18 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexen voor tabel `klant`
+--
+ALTER TABLE `klant`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `vingerid`
+--
+ALTER TABLE `vingerid`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexen voor tabel `voedselbanken`
 --
 ALTER TABLE `voedselbanken`
@@ -110,6 +140,12 @@ ALTER TABLE `voedselbanken`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT voor een tabel `vingerid`
+--
+ALTER TABLE `vingerid`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT voor een tabel `voedselbanken`
